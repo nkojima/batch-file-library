e82226e2b97877c17502127ae14488063216d046
kojiro629 <kojiro629@gmail.com>
2021-06-01 07:21:10 +0900

    Fix 'set_temporary_proxy.bat'. #4

--- set_temporary_proxy.bat
+++ set_temporary_proxy.bat
@@ -1,2 +1,13 @@
 # PROXYを一時的に設定する。
 # ※OS再起動で設定が失われる。
+
+set HTTP_PROXY=http://username:password@proxy.test.com:8080
+set HTTPS_PROXY=http://username:password@proxy.test.com:8080
+
+# PROXYの認証が不要な場合はこちら。
+set HTTP_PROXY=http://proxy.test.com:8080
+set HTTPS_PROXY=http://proxy.test.com:8080
+
+# 設定値の確認
+echo %HTTP_PROXY%
+echo %HTTPS_PROXY%
