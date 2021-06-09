746ee9890c7a6022cc3bdd945223d5593e029656
kojiro629 <kojiro629@gmail.com>
2021-06-09 23:35:26 +0900

    Add 'hardware_info.bat'. #3

---
+++ hardware_info.bat
@@ -0,0 +1,9 @@
+rem -------------------------------------------------------------------------------
+rem ハードウェア情報を取得する。
+rem
+rem [参考URL] https://webbibouroku.com/Blog/Article/cmd-cpu-os-product
+rem -------------------------------------------------------------------------------
+
+echo [CPU Info]
+wmic CPU get Name | findstr /C:"AMD" /C:"Intel"
+
