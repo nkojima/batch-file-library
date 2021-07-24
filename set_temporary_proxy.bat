a155b79ed9a486f063556d466e12f9afaeb9495f
kojiro629 <kojiro629@gmail.com>
2021-07-24 23:52:49 +0900

    Use "proxy_port" instead of "8080".

--- set_temporary_proxy.bat
+++ set_temporary_proxy.bat
@@ -1,12 +1,12 @@
 # PROXYを一時的に設定する。
 # ※OS再起動で設定が失われる。
 
-set HTTP_PROXY=http://username:password@proxy.test.com:8080
-set HTTPS_PROXY=http://username:password@proxy.test.com:8080
+set HTTP_PROXY=http://username:password@proxy.test.com:proxy_port
+set HTTPS_PROXY=http://username:password@proxy.test.com:proxy_port
 
 # PROXYの認証が不要な場合はこちら。
-set HTTP_PROXY=http://proxy.test.com:8080
-set HTTPS_PROXY=http://proxy.test.com:8080
+set HTTP_PROXY=http://proxy.test.com:proxy_port
+set HTTPS_PROXY=http://proxy.test.com:proxy_port
 
 # 設定値の確認
 echo %HTTP_PROXY%
